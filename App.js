08a3f0034aaf1fa6eee741884d55da9a42eac0b8
Hai Pham <haivan.qn@gmail.com>
2023-01-07 16:02:13 +0700

    Loading expo google fonts

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts as oswaldFont,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as latoFont, Lato_400Regular } from "@expo-google-fonts/lato";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldLoaded] = oswaldFont({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = latoFont({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
