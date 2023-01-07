97bf674bcabc3a8046a0f6f84b4f7de6c09f0c26
Hai Pham <haivan.qn@gmail.com>
2023-01-07 16:39:44 +0700

    Using SGV with Rating

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -26,6 +29,12 @@ const Address = styled.Text`
   font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
 export const RestaurentInfo = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
@@ -34,13 +43,21 @@ export const RestaurentInfo = ({ restaurant = {} }) => {
       "https://media.healthyfood.com/wp-content/uploads/2017/03/Sushi_in_10_steps.jpg",
     ],
     address = "100 some street",
-    rate = 4,
+    rating = 4,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
