c01ac65a69c9e5be253ac4a9f9f91dab052f87cb
Hai Pham <haivan.qn@gmail.com>
2023-01-08 13:22:48 +0700

    Add Open SVG Icon

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 
 import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -35,6 +36,21 @@ const Rating = styled.View`
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)`
+  flex-direction: row;
+`;
+
 export const RestaurentInfo = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
@@ -43,6 +59,7 @@ export const RestaurentInfo = ({ restaurant = {} }) => {
       "https://media.healthyfood.com/wp-content/uploads/2017/03/Sushi_in_10_steps.jpg",
     ],
     address = "100 some street",
+    isOpenNow = true,
     rating = 4,
   } = restaurant;
 
@@ -53,11 +70,16 @@ export const RestaurentInfo = ({ restaurant = {} }) => {
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {ratingArray.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-        </Rating>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isOpenNow && <Open xml={open} width={30} height={30} />}
+          </SectionEnd>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
